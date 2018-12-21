92872c163a3fb89c26643782ec09b3b64e42692e
zhangxunan <554163483@qq.com>
2018-12-21 19:10:41 +0800

    修了点bug'

--- dev-www/src/components/jianli.vue
+++ dev-www/src/components/jianli.vue
@@ -33,13 +33,7 @@
       <h3 class="sec-title">期望职位</h3>
       <div class="sec-content">
 
-        <div class="pos-select">
-          <div class="option-slted">
-            <span class="option-value">
-              web前端 &nbsp;&nbsp;&nbsp; 石家庄 &nbsp;&nbsp;&nbsp; 7k-8k &nbsp;&nbsp;&nbsp; 不限
-            </span>
-            <span class="pos-tip">切换职位</span>
-          </div>
+  
           <ul class="option-wrapper">
 
             <li
@@ -53,7 +47,7 @@
             >web前端 &nbsp;&nbsp;&nbsp; 北京 &nbsp;&nbsp;&nbsp; 15k-16k &nbsp;&nbsp;&nbsp; 不限</li>
 
           </ul>
-        </div>
+    
 
       </div>
     </div>
@@ -137,27 +131,22 @@
 
         <li class="site">
 
-          <img
+          <!-- <img
             class="site-logo"
             src="/v2/chat_v2/images/social/icon_github.png"
             alt=""
-          >
-
+          > -->
+          <svg  class="site-logo" viewBox="0 0 16 16" version="1.1" width="30" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
           <a
             class="site-link"
             href="http://github.com/aixuone"
             target="_blank"
           >http://github.com/aixuone</a>
         </li>
-
+      </ul>
+      <h3 class="sec-title">个人域名网站：</h3>
+      <ul class="sec-content">
         <li class="site">
-
-          <img
-            class="site-logo"
-            src="/v2/chat_v2/images/i_url.png"
-            alt=""
-          >
-          个人域名网站：
           <a
             class="site-link"
             href="http://daifanba.com"
